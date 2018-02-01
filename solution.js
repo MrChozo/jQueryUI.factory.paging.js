7a1cc1d7bf2598428ad83cd8a3ff207f603bdfe1
Brian A. Weston <MrChozo@gmail.com>
2018-02-01 15:55:18 -0800

    Update solution.js

--- solution.js
+++ solution.js
@@ -3,6 +3,11 @@
  * Inelegant, but it's already just jQuery.
  */
  
+// Creating pagination and nav bar
+$('#message_list').paging({limit:50, activePage:1});
+
+// Nav bar buttons weren't retaining jQuery UI button widget
+
 // Make the paging buttons stay buttonized
 var pagingNav = $("div.paging-nav a");
 pagingNav.button();
